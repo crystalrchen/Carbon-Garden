6796ac9d89c1aa6ac82f4e171951991411537f11
crystalrchen <57161879+crystalrchen@users.noreply.github.com>
2020-11-08 13:46:47 -0800

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,10 @@
+  if (middleCarbonFootprintScore >= 4) {
+    document.getElementById("plant4").style.display = "hidden";
+    document.getElementById("plant6").style.display = "hidden";
+  } else if(highCarbonFootprintScore >= 4) {
+    document.getElementById("plant3").style.display = 'none';
+    document.getElementById("plant4").style.display = 'none';
+    document.getElementById("plant5").style.display = 'none';
+    document.getElementById("plant6").style.display = 'none';
+  } else {
+  }
